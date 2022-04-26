62cfbfd1c3c007eb6e9d10531164c9344019f134
Carlos Soria <soriac367@gmail.com>
2022-04-26 13:53:07 -0500

    Function that inserts a node in a sorted linekd list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,7 +11,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *tmp = *head;
 
 	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (!new || !(*head))
 		return (NULL);
 	new->n = number;
 	for (; tmp->next; tmp = tmp->next)
