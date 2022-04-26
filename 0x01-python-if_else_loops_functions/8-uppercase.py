883f8614f40d0f87653f25f6034eeb30e8f4850f
Carlos Soria <soriac367@gmail.com>
2022-04-26 12:36:41 -0500

    Uppercase implementation

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+from re import U
+
+
 def uppercase(string):
     str1 = ""
     for i in string:
         if (ord(i) > 96 and ord(i) < 123):
             str1 += chr(ord(i) - 32)
+        else:
+            str1 += i
     print(str1)
