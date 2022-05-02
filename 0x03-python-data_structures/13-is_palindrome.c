1d6b23c29af3ab07ca34362aafcabd7dec3d3022
Carlos Soria <soriac367@gmail.com>
2022-05-02 14:22:49 -0500

    Function that checks if a linked list is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,10 +1,7 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-int recursion(listint_t **save, listint_t *node);
 /**
- * is_palindrome - Checks is a linked list is palindrome
+ * is_palindrome - Checks if a linked list is palindrome
  * @head: address of linked list
  * Return: 1 if it is palindrome, 0 otherwise
  */
@@ -14,18 +11,18 @@ int is_palindrome(listint_t **head)
 		return (1);
 	if (!(*head)->next)
 		return (1);
-	return (recursion(head, *head));
+	return (r(head, *head));
 }
 
 /**
- * recursion -  Checks if a linekd list is palindrome
+ * r -  Checks if a linekd list is palindrome
  * @head: address of linked list
  * @node: address of a node
  * Return: 1 if palindrome, 0 otherwise
  */
-int recursion(listint_t **head, listint_t *node)
+int r(listint_t **head, listint_t *node)
 {
-	if (node->next && (!recursion(head, node->next) || ((*head)->n != node->n)))
+	if (node->next && (!r(head, node->next) || ((*head)->n != node->n)))
 		return (0);
 	(*head) = (*head)->next;
 	return (1);
