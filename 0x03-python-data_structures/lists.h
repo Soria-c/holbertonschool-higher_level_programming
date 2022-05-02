69f47f0748ef2305aa5a41771044ff1f6df1ed05
Carlos Soria <soriac367@gmail.com>
2022-05-02 14:22:27 -0500

    Header file for task 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * struct listint_s - singly linked list
@@ -21,5 +22,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-
+int r(listint_t **save, listint_t *node);
 #endif /* LISTS_H */
