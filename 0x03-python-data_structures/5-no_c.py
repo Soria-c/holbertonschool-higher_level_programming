b6ce71556a874d31762b26bc605e650f00fa304b
Carlos Soria <soriac367@gmail.com>
2022-05-02 14:29:28 -0500

    Removes characters from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     new = ""
     for i in my_string:
-        if (i == "c" or i == "C"):
-            new += " "
-        else:
+        if (i not in "cC"):
             new += i
     return new
