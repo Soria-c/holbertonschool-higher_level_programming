24467624382aaff9ab2a52db5ab2c492c083e1bd
Carlos Soria <soriac367@gmail.com>
2022-05-04 10:30:51 -0500

    Function that replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """Returns sorts and prints elements in a_dictionary"""
+    if not isinstance(a_dictionary, dict):
+        return
+    a_dictionary[key] = value
+    return a_dictionary
