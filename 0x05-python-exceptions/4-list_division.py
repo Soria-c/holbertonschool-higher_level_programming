d812ae4b199ef0c7dc716996c11b8eb29098e722
Carlos Soria <soriac367@gmail.com>
2022-05-16 12:08:34 -0500

    Divides elements of two lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,11 +7,10 @@ def list_division(my_list_1, my_list_2, list_length):
             result.append(my_list_1[i] / my_list_2[i])
         except (ValueError, TypeError):
             print("wrong type")
-            result.append(0)
         except (ZeroDivisionError):
-            print("division by 0")
-            result.append(0)
+            print("division by 0") 
         except IndexError:
             print("out of range")
+        finally:
             result.append(0)
     return result
