68eb9c1b483decd73d4e1cc648a1eb5d7f98e389
Carlos Soria <soriac367@gmail.com>
2022-05-16 11:15:42 -0500

    Function that prints an integer and checks for exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    """Prints an integer, checks for exceptions"""
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as er:
+        stderr.write(f"Exception: {er}\n")
+        return False
+    return True
