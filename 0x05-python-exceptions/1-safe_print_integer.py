c6ab51a8adabf703bc76692da6780f936990a3c4
Carlos Soria <soriac367@gmail.com>
2022-05-16 12:12:37 -0500

    Now handles TypeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     """Checks for Value Error exception"""
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     return True
