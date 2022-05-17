1c8e18388003b1edb70297e65c64f6d7996465c3
Carlos Soria <soriac367@gmail.com>
2022-05-17 17:05:22 -0500

    Added doc

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,16 +4,16 @@
 
 class MagicClass:
     """Class to create a MagicClass object"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Initialize a new instance of MagicClass"""
         if (type(radius) is not int) and (type(radius) is not float):
             raise TypeError("radius must be a number")
-        self._radius = radius
+        self.radius = radius
 
     def area(self):
         """Returns the area of a given MagicClass (circle) object"""
-        return self._radius ** 2 * math.pi
+        return self.radius ** 2 * math.pi
 
     def circumference(self):
         """Returns the perimeter of a given MagicClass (circle) object"""
-        return 2 * math.pi * self._radius
+        return 2 * math.pi * self.radius
