755d3ad880a47f4411a829366a84f21d394cd3aa
Carlos Soria <soriac367@gmail.com>
2022-05-17 16:05:45 -0500

    Fixed a case for TypeError raise

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,7 +13,7 @@ class Square:
             raise TypeError("size must be an integer")
         if (size < 0):
             raise ValueError("size must be >= 0")
-        if not isinstance(position, tuple) or len(position) < 2:
+        if not isinstance(position, tuple) or len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if (position[0] < 0 or position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -46,7 +46,7 @@ class Square:
     @position.setter
     def position(self, value):
         "Setter method for __position field of a Square object"
-        if not isinstance(value, tuple) or len(value) < 2:
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if (value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
