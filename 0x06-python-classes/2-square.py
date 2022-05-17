752695c9219b8b5aa70bd6213f4f730512b4f961
Carlos Soria <soriac367@gmail.com>
2022-05-17 11:23:02 -0500

    Adding type and value verification

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""In this module a new class Square is defined"""
+
+
+class Square:
+    """ Class to create an square object"""
+    def __init__(self, size=0):
+        if not(isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
