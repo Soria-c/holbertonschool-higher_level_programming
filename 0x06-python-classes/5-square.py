9905f71198264401f572dc6d1797a9d5369102f7
Carlos Soria <soriac367@gmail.com>
2022-05-17 12:04:44 -0500

    Adding method to print a Square object

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""In this module a new class Square is defined"""
+
+
+class Square:
+    """ Class to create an square object"""
+    def __init__(self, size=0):
+        """Initializes a new instance of Square
+            Args:
+                size (int): size of a new Square
+        """
+        if not(isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Instance method to calculate the area of a given Square object"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter method for a Square object"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method for a Square object"""
+        if not(isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Method to print a Square object"""
+        if not (self.__size):
+            print()
+            return
+        print("\n".join(["#"*self.__size for i in range(self.__size)]))
