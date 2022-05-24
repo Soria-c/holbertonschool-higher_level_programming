7bee659ccecd5b1908c5fb37b49ab5fe01934ce3
Carlos Soria <soriac367@gmail.com>
2022-05-24 14:57:44 -0500

    Custom implementation of __setattr__()

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""In this module a class LockedClass is defined"""
+
+
+class LockedClass(object):
+    """Class to create a new LockedCLass object"""
+    def __setattr__(self, name, value):
+        """Custom implementation of the __setattr__ magic method"""
+        if name == "first_name":
+            self.__dict__[name] = value
+        else:
+            n = "LockedClass"
+            raise AttributeError(f"'{n}' object has no attribute '{name}'")
