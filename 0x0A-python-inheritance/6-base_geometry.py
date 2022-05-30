2c6b6244f3b37e22deb6b4a0190976e37c667be8
Carlos Soria <soriac367@gmail.com>
2022-05-30 11:19:22 -0500

    Added a method to BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""In this module the class BaseGeometry is defined"""
+
+
+class BaseGeometry:
+    """Class to create a new BaseGeometry object"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
