f822203bf6d81dfa517d687793aa1da95d76920e
Carlos Soria <soriac367@gmail.com>
2022-05-30 09:27:52 -0500

    Checks if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""In this module the function is_same_class is defined"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if an object is an instance following the inheritance chain"""
+    return isinstance(obj, a_class)
