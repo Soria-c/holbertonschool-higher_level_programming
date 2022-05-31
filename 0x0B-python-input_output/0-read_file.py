ab4bface31aa971f4e6bb75e54b0712c4cf8e151
Carlos Soria <soriac367@gmail.com>
2022-05-31 14:10:08 -0500

    Fixed print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@
 def read_file(filename=""):
     """Opens a file, reads it, and prints its contents"""
     with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
