aac8bf712aaf94d4334d4b6e6128f348965a49c7
Carlos Soria <soriac367@gmail.com>
2022-05-30 19:21:08 -0500

    Fixed style

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,4 +11,4 @@ class Square(Rectangle):
         super().__init__(size, size)
 
     def __str__(self):
-       return f'[Square] {self._Rectangle__width}/{self._Rectangle__height}'
+        return f'[Square] {self._Rectangle__width}/{self._Rectangle__height}'
