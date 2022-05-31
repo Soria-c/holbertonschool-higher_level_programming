a993cdba32c649f6fd2203eb9fcb8e3ec892e544
Carlos Soria <soriac367@gmail.com>
2022-05-31 10:30:20 -0500

    Enhanced to_json instance method

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+In this module the class:
+    Student
+    is defined
+"""
+
+
+class Student:
+    """Class to create a new Student object"""
+    def __init__(self, first_name, last_name, age):
+        """Implementation of the __init__ magic method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary for self attributes"""
+        if (not attrs or not isinstance(attrs, list) or
+                [i for i in attrs if not isinstance(i, str)]):
+            return self.__dict__
+        return {i: j for i, j in self.__dict__.items() if i in attrs}
