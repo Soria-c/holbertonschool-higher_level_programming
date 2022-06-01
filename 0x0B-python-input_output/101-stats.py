8cc780e19dbe5314d28562868486c0dc911d2f76
Carlos Soria <soriac367@gmail.com>
2022-06-01 00:39:18 -0500

    Checked for a valid input

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -28,6 +28,8 @@ signal.signal(signal.SIGINT, print_data)
 for i in stdin:
     count += 1
     line = i.strip("\n").split(" ")
+    if (len(line) != 9):
+        continue
     total_size += int(line[-1])
     if line[-2] in status_code:
         status_code[line[-2]] += 1
