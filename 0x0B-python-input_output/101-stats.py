189c2545e85a800609dd37b99d4be5683219f4d4
Carlos Soria <soriac367@gmail.com>
2022-05-31 19:02:44 -0500

    Test

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -19,7 +19,8 @@ def print_data(signal=None, frame=None):
     for j, k in status_code.items():
         if (k):
             print(f"{j}: {k}")
-
+    if (signal and frame):
+        exit()
 
 
 signal.signal(signal.SIGINT, print_data)
