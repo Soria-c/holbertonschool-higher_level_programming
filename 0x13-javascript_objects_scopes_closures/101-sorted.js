71830a35885bff9e8b73a2d9dcb3b83e4301e622
Carlos Soria <soriac367@gmail.com>
2022-08-03 13:04:30 -0500

    Refactor

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-const values = Object.values(dict);
-const kys = Object.keys(dict);
-const uniq = values.filter((val, ind) => values.indexOf(val) === ind).sort();
 const r = {};
 
-for (const i of uniq) {
-  r[i] = kys.filter(x => dict[x] === i).sort((a, b) => parseInt(a) - parseInt(b));
+for (const i of new Set(Object.values(dict))) {
+  r[i] = Object.keys(dict).filter(x => dict[x] === i).sort((a, b) => parseInt(a) - parseInt(b));
 }
 console.log(r);
