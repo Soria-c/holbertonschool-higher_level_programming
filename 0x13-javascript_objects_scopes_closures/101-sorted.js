2742ef40382a3e2f669a3d12d1f5747508be0598
Carlos Soria <soriac367@gmail.com>
2022-08-03 10:18:20 -0500

    Script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-const val = Object.values(dict);
+const values = Object.values(dict);
 const kys = Object.keys(dict);
-const uniq = val.filter((val, ind, arr) => arr.indexOf(val) === ind);
+const uniq = values.filter((val, ind) => values.indexOf(val) === ind).sort();
 const r = {};
 
 for (const i of uniq) {
   r[i] = kys.filter(x => dict[x] === i);
 }
 console.log(r);
+
+
