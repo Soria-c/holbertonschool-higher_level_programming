5d6399fd44dba592af644f0b40946afe0060bb29
Carlos Soria <soriac367@gmail.com>
2022-08-06 10:57:45 -0500

    Script that lists all City objects from a given database

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that lists all City objects from a given database"""
+
+
+if __name__ == '__main__':
+    from relationship_city import City, Base
+    from relationship_state import State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.engine.url import URL
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+
+    conn_params = {
+        'drivername': 'mysql+mysqldb',
+        'username': argv[1],
+        'password': argv[2],
+        'host': 'localhost',
+        'database': argv[3]
+    }
+
+    engine = create_engine(URL.create(**conn_params), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        re = session.query(City).order_by(City.id).all()
+        for i in re:
+            print(f"""{i.id}: {i.name} ->
+            {session.query(State).filter_by(id=i.state_id).first().name}""")
