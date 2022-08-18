7ac85217e390deae13bb033bd6e828f153d33ca5
Carlos Soria <soriac367@gmail.com>
2022-08-18 12:09:38 -0500

    Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# curl -Is $1 | grep -Po 'Content-Length:[[:blank:]]\K([0-9]+)'
-
-regex="Content-Length:[[:blank:]]([0-9]+)"
-response=$(curl -Is "$1")
-if [[ $response =~ $regex ]]; then
-        echo "${BASH_REMATCH[1]}"
-fi
+regex="Content-Length:[[:blank:]]([0-9]+)"; response=$(curl -Is "$1"); if [[ $response =~ $regex ]]; then echo "${BASH_REMATCH[1]}"; fi
