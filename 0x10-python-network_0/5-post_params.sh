f8e5f53014ec3b72095563823d8b9e6c8e3d92bc
Carlos Soria <soriac367@gmail.com>
2022-08-18 14:54:39 -0500

    Script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -X POST "$1/?email=test@gmail%2Ecom&subject=I%20will%20always%20be%20here%20for%20PLD"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
