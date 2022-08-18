1b517a51fac19a6fdf1329e8fb74f73bf55168b3
Carlos Soria <soriac367@gmail.com>
2022-08-18 16:08:34 -0500

    Script making a request with a data payload and a custom Origin header

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bashs
 # Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -Ls -d "user_id=98" -H "Origin: HolbertonSchool" -X PUT 0.0.0.0:5000/catch_me
+curl -sL -d "user_id=98" -H "Origin: HolbertonSchool" -X PUT 0.0.0.0:5000/catch_me
