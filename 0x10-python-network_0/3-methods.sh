7ee1e2293e8696354a4429f4fb9ebb3e3f2af9d3
Carlos Soria <soriac367@gmail.com>
2022-08-18 12:51:57 -0500

    Script that takes in a URL and displays all HTTP methods the server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept
-curl -iLs -X OPTIONS "$1" | grep -Po "allow:[[:blank:]]\K([[:upper:]]+(,[[:blank:]])?)+"
+curl -ILs "$1" | grep -Po "(Allow|allow):[[:blank:]]\K([[:upper:]]+(,[[:blank:]])?)+"
