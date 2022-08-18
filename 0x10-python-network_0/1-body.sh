998b40b802ed79d84b0b7b8ba2364656aa30f12a
Carlos Soria <soriac367@gmail.com>
2022-08-18 16:12:30 -0500

    Script that takes in a URL, sends a GET request to the URL, and displays the body of the response only if it is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to the URL, and displays the body of the response only if it is 200
-regex="HTTP/(1.1|2)[[:blank:]]([0-9]{3})";response=$(curl -Is "$1");if [[ $response =~ $regex ]]; then if [ "${BASH_REMATCH[2]}" -eq 200 ]; then curl -s "$1"; fi; fi
+curl -sL "$1"
