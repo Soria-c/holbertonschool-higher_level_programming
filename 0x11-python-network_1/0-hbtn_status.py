ca805025d1aec3d96544f2eb80c7fa16b9641174
Carlos Soria <soriac367@gmail.com>
2022-08-19 09:20:23 -0500

    Script to make a get request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script to make a get request"""
+import urllib.request as req
+
+r = req.Request('https://intranet.hbtn.io/status')
+with req.urlopen(r) as response:
+    body = response.read()
+    print(f"""Body response:
+    - type: {type(body)}
+    - content: {body}
+    - utf8 content: {body.decode()}""")
