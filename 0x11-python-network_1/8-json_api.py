bce4302ff058b9b0a5e2e62e1226d60b410abc2c
Carlos Soria <soriac367@gmail.com>
2022-08-19 11:45:50 -0500

    Script to make a post request to get a json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script to make a post request to get a json"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    import json
+
+    kwargs = {
+        'url': 'http://0.0.0.0:5000/search_user',
+        'data': {
+            'q': argv[1] if len(argv) >= 2 else ""
+        }
+    }
+
+    response = requests.post(**kwargs)
+    try:
+        js_on = json.loads(response.text)
+        if (js_on):
+            print(f"[{js_on.get('id')}] {js_on.get('name')}")
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
