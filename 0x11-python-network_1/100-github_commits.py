ff52d299c4d7a3bacb783c5ba6216ff1f4f9f85a
Carlos Soria <soriac367@gmail.com>
2022-08-19 13:51:02 -0500

    Script to access the GitHub API

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     kwargs = {
         'url': 'https://api.github.com/repos/{}/{}/commits'
-        .format(argv[2], argv[1])
+        .format(argv[1], argv[2])
     }
     response = requests.get(**kwargs)
     j = response.json()
