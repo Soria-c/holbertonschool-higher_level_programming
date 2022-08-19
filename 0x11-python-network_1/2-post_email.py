e09b64ce1e513cf1a6ef6ac27f72465406b693ac
Carlos Soria <soriac367@gmail.com>
2022-08-19 09:45:26 -0500

    Script to make a post request to a given url

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script to make a post request to a given url"""
+import urllib.request as req
+import urllib.parse as parse_data
+from sys import argv
+
+
+payload = {
+    'email': argv[2]
+}
+
+kwargs = {
+    'url': argv[1],
+    'data': parse_data.urlencode(payload).encode()
+}
+r = req.Request(**kwargs)
+with req.urlopen(r) as response:
+    print(response.read().decode())
