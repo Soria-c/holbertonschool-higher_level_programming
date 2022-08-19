ec8e60d5de1be888809f18df2be890dd45ad62ee
Carlos Soria <soriac367@gmail.com>
2022-08-19 13:16:15 -0500

    Script to make a get request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     r = req.Request('https://intranet.hbtn.io/status')
     with req.urlopen(r) as response:
         body = response.read()
-        print(f"""Body response:
-        - type: {type(body)}
-        - content: {body}
-        - utf8 content: {body.decode()}""")
+        print('Body response:')
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode('utf-8')))
