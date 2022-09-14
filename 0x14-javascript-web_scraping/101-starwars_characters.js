411756e3e42565526917571622cb1750256be9ec
Carlos Soria <soriac367@gmail.com>
2022-09-13 22:30:24 -0500

    Consume an API

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const axios = require('axios');
+
+const promises = [];
+axios.defaults.baseURL = 'https://swapi-api.hbtn.io/api/films/';
+axios
+  .get(process.argv[2])
+  .then((r) => {
+    for (const i of r.data.characters) {
+      promises.push(axios.get(i));
+    }
+    Promise.all(promises)
+      .then((r) => {
+        for (const j of r) {
+          console.log(j.data.name);
+        }
+      })
+      .carch((e) => console.log(e));
+  })
+  .catch((e) => console.log(e));
