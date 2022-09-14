9755926e0535d3536bb60861b6f76410ac9ef338
Carlos Soria <soriac367@gmail.com>
2022-09-13 21:04:59 -0500

    Added catch

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 const axios = require('axios');
 
-axios.get(process.argv[2]).then((r) => {
-  console.log(`code: ${r.status}`);
-});
+axios
+  .get(process.argv[2])
+  .then((r) => {
+    console.log(`code: ${r.status}`);
+  })
+  .catch((e) => {
+    console.log(`code: ${e.response.status}`);
+  });
