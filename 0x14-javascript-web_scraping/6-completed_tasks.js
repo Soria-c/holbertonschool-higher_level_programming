b5c2ac910a435831fae1a373c2730c5376ab918a
Carlos Soria <soriac367@gmail.com>
2022-09-13 21:58:42 -0500

    Consume an API

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,12 +2,15 @@
 const axios = require('axios');
 
 const users = {};
-axios.get(process.argv[2]).then((r) => {
-  for (const i of r.data) {
-    if (!Object.prototype.hasOwnProperty.call(users, i.userId)) users[i.userId] = 0;
-    if (i.completed) users[i.userId] += 1;
-  }
-  console.log(users);
-}).catch((e) => {
-  if (e) console.log(e);
-});
+axios
+  .get(process.argv[2])
+  .then((r) => {
+    for (const i of r.data) {
+      if (i.completed) {
+        if (!Object.prototype.hasOwnProperty.call(users, i.userId)) users[i.userId] = 0;
+        users[i.userId] += 1;
+      }
+    }
+    console.log(users);
+  })
+  .catch((e) => console.log(e));
